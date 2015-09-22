f71922e22184a6d6d96cfa3cf21b80f6c68d54f6
Sergo Bushmakin <giant2788@yahoo.com>
2015-09-22 02:00:37 +0000

    completed all tasks

--- task_02.py
+++ task_02.py
@@ -4,3 +4,15 @@
 
 
 import inquisition
+
+FLEMISH_INDEX = inquisition.SPANISH.index('Spanish')
+
+FLEMISH_LEN = len('Spanish')
+
+FLEMISH_SLICE1 = inquisition.SPANISH[:19]
+
+FLEMISH_SLICE2 = inquisition.SPANISH[19:26]
+
+FLEMISH_SLICE3 = inquisition.SPANISH[26:]
+
+FLEMISH = FLEMISH_SLICE1 + 'Flemish' + FLEMISH_SLICE3
